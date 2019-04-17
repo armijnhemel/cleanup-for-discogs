b6911aeb707caf7f4ae383e91ddfb1b0ba0608c8
Armijn Hemel <armijn@tjaldur.nl>
2019-04-17 18:41:05 +0200

    discogsmonthdiff.py: more docs

--- discogsmonthdiff.py
+++ discogsmonthdiff.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 # Quick and dirty script to see where differences in month are and where
 # new errors were introduced in old releases. Only catches releases for
@@ -10,25 +10,32 @@
 #
 # Copyright 2018-2019 - Armijn Hemel
 
-oldmonth = open('/home/armijn/discogs-data/may2018.txt', 'r')
-newmonth = open('/home/armijn/discogs-data/june2018.txt', 'r')
+oldmonth = open('/tmp/march2019.txt', 'r')
+#newmonth = open('/home/armijn/discogs-data/february2019.txt', 'r')
+newmonth = open('/tmp/april2019.txt', 'r')
 
+# store the old releases, as a list, so order is kept
 oldreleases = []
 
 for i in oldmonth:
+    oldrelease = int(i.strip().split('https://www.discogs.com/release/')[-1])
     if 'Tracklisting' in i:
         continue
     if 'Role' in i:
         continue
     if 'Artist' in i:
         continue
-    oldreleases.append(int((i.strip().split('https://www.discogs.com/release/')[-1])))
+    oldreleases.append(oldrelease)
 
+# store the latest wrong release in the old release set. This is used
+# as a cut off value. It (falsely) assumes that this is the last release
+# but it could very well be that this is not the case (and this is because
+# Discogs does not include when a release was added in the XML data dump.
 latestoldrelease = oldreleases[-1]
 
 oldreleasesset = set(oldreleases)
 
-newreleases = set()
+newreleases = []
 
 for i in newmonth:
     newrelease = int(i.strip().split('https://www.discogs.com/release/')[-1])
@@ -43,13 +50,15 @@ for i in newmonth:
     if newrelease not in oldreleasesset:
         pass
         #print(i.strip())
-    newreleases.add(newrelease)
-
-#print(len(newreleases))
-
-for i in oldreleases:
-    if i not in newreleases:
-        print(' -- https://www.discogs.com/release/%d' % i)
+    newreleases.append(newrelease)
 
 oldmonth.close()
 newmonth.close()
+
+# Now check for each of the new releases if they are present
+# in the set of old releases. If not, it is a newly introduced
+# error and should be reported. Outputs data that can be fed
+# into makecharts.py
+for i in newreleases:
+    if i not in oldreleasesset:
+        print(' -- https://www.discogs.com/release/%d' % i)
