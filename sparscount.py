df5ac06b2d37932566e56e46d28fae40f3492416
Armijn Hemel <armijn@tjaldur.nl>
2023-12-28 12:47:00 +0100

    sparscount.py: pylint-3 fixes

--- sparscount.py
+++ sparscount.py
@@ -8,66 +8,50 @@
 
 
 import collections
-import sys
-import os
 import re
 
-sparsfilename = "dl.txt"
+spars_filename = "dl.txt"
 seps = ['.', '·', '•', '∙']
 
 sparsregex = []
-rx0 = re.compile('([ad]\s*[ad]\s*[ad])$')
-rx1 = re.compile('\[([ad]{3})\]$')
-rx2 = re.compile('([ad]{3}),\s*([ad]{3})$')
-rx3 = re.compile('([ad]{3})\s*/\s*([ad]{3})$')
-rx4 = re.compile('([ad]{3})\s*&\s*([ad]{3})$')
-rx5 = re.compile('([ad]{3})\s*\|\s*([ad]{3})$')
-rx6 = re.compile('([ad]{3})\s*\+\s*([ad]{3})$')
-rx7 = re.compile('([ad]{3})\s*/\s*([ad]{3})$')
-rx8 = re.compile('([ad]\s*/\s*[ad]\s*/\s*[ad])$')
-rx9 = re.compile('([ad]\s*-\s*[ad]\s*-\s*[ad])$')
-rx10 = re.compile('\|?([ad]\s*\|\s*[ad]\s*\|\s*[ad])\|?$')
-rx11 = re.compile('([ad]{3})\s*([ad]{3})$')
-rx12 = re.compile('([ad]{3})\s*-\s*([ad]{3})$')
-rx13 = re.compile('([ad]{3}),?\s*([ad]{3}),?\s*([ad]{3})$')
-rx14 = re.compile('([ad]{3})\s*/\s*([ad]{3})\s*/\s*([ad]{3})$')
-rx15 = re.compile('([ad]{3})\s*-\s*([ad]{3})\s*-\s*([ad]{3})$')
-rx16 = re.compile('(\[[ad]\]\s*\[[ad]\]\s*\[[ad]\])$')
-rx17 = re.compile('([ad]{4})$')
+rx0 = re.compile(r'([ad]\s*[ad]\s*[ad])$')
+rx1 = re.compile(r'\[([ad]{3})\]$')
+rx2 = re.compile(r'([ad]{3}),\s*([ad]{3})$')
+rx3 = re.compile(r'([ad]{3})\s*/\s*([ad]{3})$')
+rx4 = re.compile(r'([ad]{3})\s*&\s*([ad]{3})$')
+rx5 = re.compile(r'([ad]{3})\s*\|\s*([ad]{3})$')
+rx6 = re.compile(r'([ad]{3})\s*\+\s*([ad]{3})$')
+rx7 = re.compile(r'([ad]{3})\s*/\s*([ad]{3})$')
+rx8 = re.compile(r'([ad]\s*/\s*[ad]\s*/\s*[ad])$')
+rx9 = re.compile(r'([ad]\s*-\s*[ad]\s*-\s*[ad])$')
+rx10 = re.compile(r'\|?([ad]\s*\|\s*[ad]\s*\|\s*[ad])\|?$')
+rx11 = re.compile(r'([ad]{3})\s*([ad]{3})$')
+rx12 = re.compile(r'([ad]{3})\s*-\s*([ad]{3})$')
+rx13 = re.compile(r'([ad]{3}),?\s*([ad]{3}),?\s*([ad]{3})$')
+rx14 = re.compile(r'([ad]{3})\s*/\s*([ad]{3})\s*/\s*([ad]{3})$')
+rx15 = re.compile(r'([ad]{3})\s*-\s*([ad]{3})\s*-\s*([ad]{3})$')
+rx16 = re.compile(r'(\[[ad]\]\s*\[[ad]\]\s*\[[ad]\])$')
+rx17 = re.compile(r'([ad]{4})$')
 
-sparsregex.append(rx0)
-sparsregex.append(rx1)
-sparsregex.append(rx2)
-sparsregex.append(rx3)
-sparsregex.append(rx4)
-sparsregex.append(rx5)
-sparsregex.append(rx6)
-sparsregex.append(rx7)
-sparsregex.append(rx8)
-sparsregex.append(rx9)
-sparsregex.append(rx10)
-sparsregex.append(rx11)
-sparsregex.append(rx12)
-sparsregex.append(rx13)
-sparsregex.append(rx14)
-sparsregex.append(rx15)
-sparsregex.append(rx16)
-sparsregex.append(rx17)
+for i in [rx0, rx1, rx2, rx3, rx4, rx5, rx6,
+          rx7, rx8, rx9, rx10, rx11, rx12, rx13,
+          rx14, rx15, rx16, rx17]:
+    sparsregex.append(i)
 
-sparscounter = collections.Counter()
+spars_counter = collections.Counter()
 
 total = 0
 
-for i in open(sparsfilename, 'r'):
+for i in open(spars_filename, 'r'):
     if not i.startswith('SPARS'):
-       continue
+        continue
     total += 1
-    sparscomponent = i.strip()[6:].rsplit(' ', 1)[0].strip().lower()
+    spars_component = i.strip()[6:].rsplit(' ', 1)[0].strip().lower()
     for s in seps:
-        sparscomponent = sparscomponent.replace(s, '')
-    sparsfound = False
+        spars_component = spars_component.replace(s, '')
+    spars_found = False
     for rx in sparsregex:
-        res = rx.match(sparscomponent)
+        res = rx.match(spars_component)
         if res is not None:
             newres = []
             for n in res.groups():
@@ -78,13 +62,13 @@ for i in open(sparsfilename, 'r'):
                 n = n.replace('[', '')
                 n = n.replace(']', '')
                 newres.append(n)
-            sparscounter.update(newres)
-            sparsfound = True
+            spars_counter.update(newres)
+            spars_found = True
             break
-    if not sparsfound:
-        print(sparscomponent)
+    if not spars_found:
+        print(spars_component)
 
-for i in sparscounter.most_common():
+for i in spars_counter.most_common():
     print(i)
 
 print(total)
