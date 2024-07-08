6e6ee065302d281cb912b7a196dd2a22bd13143b
Armijn Hemel <armijn@tjaldur.nl>
2024-07-08 15:20:07 +0200

    pylint fixes

--- labelcount.py
+++ labelcount.py
@@ -10,47 +10,48 @@
 import collections
 import re
 
-labelfilename = "label.txt"
+label_filename = "label.txt"
 
-labelregex = []
+label_regex = []
 rx0 = re.compile(r'(?:lc)[\s/\-]*(\d{4,6})')
 
-labelregex.append(rx0)
+label_regex.append(rx0)
 
-labelcounter = collections.Counter()
+label_counter = collections.Counter()
 
 total = 0
 valid = 0
 ignored = 0
 
-for i in open(labelfilename, 'r'):
-    if not i.startswith('LABEL CODE'):
-        continue
-    total += 1
-    labelcomponent = i.strip()[10:].strip().lower()
-    labelcodefound = False
-    for rx in labelregex:
-        res = rx.match(labelcomponent)
-        if res is not None:
-            labelcode = res.groups()[0]
-            if len(labelcode) == 4:
-                labelcode = "00" + labelcode
-            elif len(labelcode) == 5:
-                labelcode = "0" + labelcode
-            labelcounter.update([labelcode])
-            labelcodefound = True
-            break
-    if labelcodefound:
-        valid += 1
-    else:
-        ignored += 1
+with open(label_filename, 'r') as label_file:
+    for i in label_file:
+        if not i.startswith('LABEL CODE'):
+            continue
+        total += 1
+        label_component = i.strip()[10:].strip().lower()
+        label_code_found = False
+        for rx in label_regex:
+            res = rx.match(label_component)
+            if res is not None:
+                label_code = res.groups()[0]
+                if len(label_code) == 4:
+                    label_code = "00" + label_code
+                elif len(label_code) == 5:
+                    label_code = "0" + label_code
+                label_counter.update([label_code])
+                label_code_found = True
+                break
+        if label_code_found:
+            valid += 1
+        else:
+            ignored += 1
 
 print(f"TOTAL: {total}")
 print(f"VALID: {valid}")
 print(f"IGNORED: {ignored}")
-print(f"Labels: {len(labelcounter)}")
+print(f"Labels: {len(label_counter)}")
 
 counter = 1
-for label, amount in labelcounter.most_common():
+for label, amount in label_counter.most_common():
     print(f"{counter} LC {label}: {amount}")
     counter += 1
